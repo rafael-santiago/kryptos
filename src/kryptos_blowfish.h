5f4653e9f830d3494e3d5f56915660995709254b
rafael-santiago <voidbrainvoid@gmail.com>
2017-04-13 10:16:57 -0800

    Adding cipher: Blowfish

---
+++ src/kryptos_blowfish.h
@@ -0,0 +1,20 @@
+/*
+ *                          Copyright (C) 2006, 2017 by Rafael Santiago
+ *
+ * This is a free software. You can redistribute it and/or modify under
+ * the terms of the GNU General Public License version 2.
+ *
+ */
+#ifndef KRYPTOS_KRYPTOS_BLOWFISH_H
+#define KRYPTOS_KRYPTOS_BLOWFISH_H 1
+
+#include <kryptos_types.h>
+
+#define KRYPTOS_BLOWFISH_BLOCKSIZE 8
+
+void kryptos_blowfish_cipher(kryptos_task_ctx **ktask);
+
+void kryptos_blowfish_setup(kryptos_task_ctx *ktask, kryptos_u8_t *key, const size_t key_size,
+                            const kryptos_cipher_mode_t mode);
+
+#endif
