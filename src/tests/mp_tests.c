eeb18444ac1728acd72546e20018a517da0112b7
rafael-santiago <voidbrainvoid@gmail.com>
2017-07-16 08:02:29 -0800

    Improving on the test message ;)

--- src/tests/mp_tests.c
+++ src/tests/mp_tests.c
@@ -2057,7 +2057,7 @@ CUTE_TEST_CASE(kryptos_mp_gen_prime_tests)
     }
     CUTE_ASSERT(p->data_size == expected_bitsize);
     printf(" *** Your luck number is: "); kryptos_print_mp(p);
-    printf(" *** Watch for it everywhere...\n");
+    printf(" *** Search for it in everywhere...\n");
     kryptos_del_mp_value(p);
     // INFO(Rafael): Well, all we need to do is to believe in this function... To test the return to make sure if the
     //               value is really prime means to use the same tests (Fermat, Miller-Rabin) used by the generating function.
