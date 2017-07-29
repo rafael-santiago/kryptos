5f863b0473a8223af07f9cb56901c4ed34dec32d
Rafael Santiago <voidbrainvoid@gmail.com>
2017-07-29 13:11:37 +0200

    Enabling kryptos_pem_get_data_tests and kryptos_pem_put_data_tests (kernel mode)

--- src/tests/kernel/ktests.c
+++ src/tests/kernel/ktests.c
@@ -27,8 +27,8 @@ int ktest_monkey(void) {
     KUTE_RUN_TEST(kryptos_base64_tests);
     KUTE_RUN_TEST(kryptos_uuencode_tests);
     //KUTE_RUN_TEST(kryptos_huffman_tests);
-    //KUTE_RUN_TEST(kryptos_pem_get_data_tests);
-    //KUTE_RUN_TEST(kryptos_pem_put_data_tests);
+    KUTE_RUN_TEST(kryptos_pem_get_data_tests);
+    KUTE_RUN_TEST(kryptos_pem_put_data_tests);
 
 
     //KUTE_RUN_TEST(kryptos_pem_get_mp_data_tests);
