7f818930b94547d7683b2b61857fbddb4095347b
Rafael Santiago <voidbrainvoid@gmail.com>
2017-08-03 14:58:48 +0200

    AES/CBC HMAC (kernel mode)

--- src/tests/kernel/hash_tests.c
+++ src/tests/kernel/hash_tests.c
@@ -471,6 +471,18 @@ KUTE_TEST_CASE(kryptos_hmac_tests)
     kryptos_run_hmac_tests(t, plaintext, plaintext_size, aes, ripemd128, key, key_size, kKryptosECB);
     kryptos_run_hmac_tests(t, plaintext, plaintext_size, aes, ripemd160, key, key_size, kKryptosECB);
 
+    // INFO(Rafael): AES/CBC.
+
+    kryptos_run_hmac_tests(t, plaintext, plaintext_size, aes, sha1, key, key_size, kKryptosCBC);
+    kryptos_run_hmac_tests(t, plaintext, plaintext_size, aes, sha224, key, key_size, kKryptosCBC);
+    kryptos_run_hmac_tests(t, plaintext, plaintext_size, aes, sha256, key, key_size, kKryptosCBC);
+    kryptos_run_hmac_tests(t, plaintext, plaintext_size, aes, sha384, key, key_size, kKryptosCBC);
+    kryptos_run_hmac_tests(t, plaintext, plaintext_size, aes, sha512, key, key_size, kKryptosCBC);
+    kryptos_run_hmac_tests(t, plaintext, plaintext_size, aes, md4, key, key_size, kKryptosCBC);
+    kryptos_run_hmac_tests(t, plaintext, plaintext_size, aes, md5, key, key_size, kKryptosCBC);
+    kryptos_run_hmac_tests(t, plaintext, plaintext_size, aes, ripemd128, key, key_size, kKryptosCBC);
+    kryptos_run_hmac_tests(t, plaintext, plaintext_size, aes, ripemd160, key, key_size, kKryptosCBC);
+
 
 #undef kryptos_run_hmac_tests
 
