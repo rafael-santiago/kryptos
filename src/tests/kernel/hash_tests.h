6c8182529075a61c04ab59ef7f59bf37511f1271
Rafael Santiago <voidbrainvoid@gmail.com>
2017-08-03 12:31:02 +0200

    Copyright disclaimer

--- src/tests/kernel/hash_tests.h
+++ src/tests/kernel/hash_tests.h
@@ -1,3 +1,10 @@
+/*
+ *                                Copyright (C) 2017 by Rafael Santiago
+ *
+ * This is a free software. You can redistribute it and/or modify under
+ * the terms of the GNU General Public License version 2.
+ *
+ */
 #ifndef KRYPTOS_KRYPTOS_TEST_KERNEL_HASH_TESTS_H
 #define KRYPTOS_KRYPTOS_TEST_KERNEL_HASH_TESTS_H 1
 
