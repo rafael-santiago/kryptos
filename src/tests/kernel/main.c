08ab9643a2ac89984fe43ac5b107c8cd91b556e6
rafael-santiago <voidbrainvoid@tutanota.com>
2019-05-01 11:45:14 -0800

    Enabling  in kernel mode tests

--- src/tests/kernel/main.c
+++ src/tests/kernel/main.c
@@ -118,7 +118,7 @@ KUTE_TEST_CASE(ktest_monkey)
     KUTE_RUN_TEST(kryptos_pbkdf2_macro_tests);
     KUTE_RUN_TEST(kryptos_do_argon2_tests);
     KUTE_RUN_TEST(kryptos_argon2_macro_tests);
-    //KUTE_RUN_TEST(kryptos_do_argon2_bounds_tests);
+    KUTE_RUN_TEST(kryptos_do_argon2_bounds_tests);
     //KUTE_RUN_TEST(kryptos_argon2_macro_bounds_tests);
 
     KUTE_RUN_TEST(kryptos_mp_new_value_tests);
