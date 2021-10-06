5c501bc035529e9394b32e008800549655c7d84d
rafael-santiago <voidbrainvoid@tutanota.com>
2021-10-06 09:04:05 -0300

    Submodule update [libcutest]

--- src/tests/kernel/main.c
+++ src/tests/kernel/main.c
@@ -33,8 +33,8 @@ KUTE_DECLARE_TEST_CASE(ktest_monkey);
 
 KUTE_TEST_CASE(ktest_monkey)
     KUTE_RUN_TEST(kryptos_memset_tests);
-    /*KUTE_RUN_TEST(kryptos_padding_tests);
-    KUTE_RUN_TEST(kryptos_unbiased_rand_mod_u8_tests);
+    KUTE_RUN_TEST(kryptos_padding_tests);
+    /*KUTE_RUN_TEST(kryptos_unbiased_rand_mod_u8_tests);
     KUTE_RUN_TEST(kryptos_unbiased_rand_mod_u16_tests);
     KUTE_RUN_TEST(kryptos_unbiased_rand_mod_u32_tests);
     KUTE_RUN_TEST(kryptos_unbiased_rand_mod_u64_tests);
